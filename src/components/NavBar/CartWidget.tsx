b8cc9e9e6c846ff5176bd8aef4fc5ff0c7a69090
cotanol <salcedomoralesfranciscojose@gmail.com>
2024-12-18 16:09:15 -0500

    Test

--- src/components/NavBar/CartWidget.tsx
+++ src/components/NavBar/CartWidget.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { BsCart4 } from "react-icons/bs";
 import { ShoppingCartContext } from "../../context";
+import { Link } from "react-router-dom";
 
 const IconoCarro = () => {
 
@@ -8,7 +9,7 @@ const IconoCarro = () => {
 
     return (
         <div className="relative flex items-center">
-            <a href=""><BsCart4 className="text-white text-2xl"/></a>
+            <Link to="/"><BsCart4 className="text-white text-2xl"/></Link>
             <p className="absolute top-[-5px] right-[-5px] bg-red-600 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">{context.count}</p>
         </div>
         
